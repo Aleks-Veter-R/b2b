15a8243654824d686e695a1c3738561c590d2d7f
Aleksander <leks.creative.mail@gmail.com>
2025-05-07 15:02:55 +0300

    B2B-021. Добавлен новый тип Check для компоненты Icon

---
+++ src/components/components/icon/images/Check.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+const Check: FunctionComponent = () => {
+    return (
+        <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 4.67347L5.125 8.71429L13 1" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    );
+};
+
+export default Check;

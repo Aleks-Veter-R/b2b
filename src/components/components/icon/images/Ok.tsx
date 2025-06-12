e35cce4b5af42115d587d2793200227e381f6fcb
Aleksander <leks.creative.mail@gmail.com>
2025-06-12 12:39:13 +0300

    B2B-033. Part - 2. Add new icon type: 'Ok'

---
+++ src/components/components/icon/images/Ok.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+
+const Ok: FunctionComponent = () => {
+    return (    
+        <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" fill="#EFD90F"/>
+            <line x1="33.5349" y1="58.0266" x2="51.5349" y2="72.0266" stroke="black" stroke-width="5"/>
+            <line x1="47.9805" y1="72.5263" x2="74.9805" y2="35.5263" stroke="black" stroke-width="5"/>
+        </svg>
+    );
+};
+
+export default Ok;
